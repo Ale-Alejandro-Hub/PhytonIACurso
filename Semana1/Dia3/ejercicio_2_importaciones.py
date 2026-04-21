09a573f774578f6c7f54628653546c4d618501b0
Alejandro <aloeza23@gmail.com>
2026-04-21 15:35:37 -0600

    Delete Semana1 directory

--- Semana1/Dia3/ejercicio_2_importaciones.py
+++
@@ -1,9 +0,0 @@
-import day3_ejercicio2 as ejer2
-
-palabras = "Voy a cantar un corrido, escuchen muy bien mis compas"
-palabras2 = "que tal como andamos"
-palabras3 = "No se si esto es un palindromo o no"
-
-print(ejer2.invertir_cadena(palabras))
-print(ejer2.contar_vocales(palabras2))
-print(ejer2.es_palindromo(palabras3))
